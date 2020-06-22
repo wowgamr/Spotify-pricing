d6dc9b02a8d2d54eacfa5e3a63e1051c2a60b5f4
wowgamr <wowgamr@yandex.ru>
2020-06-22 13:38:14 +0300

    Update selectors

--- crawler.php
+++ crawler.php
@@ -41,8 +41,8 @@ function getPrice($url) {
             $price = preg_replace('/[^,.0-9]/', '', $price);
             $price = preg_replace('/.00$/', '', $price); // beautify price
         }
-        elseif (isset($dom->select('.eOPyHF')[0]['text'])){
-            $price = $dom->select('.eOPyHF')[0]['text'];
+        elseif (isset($dom->select('.crfFDZ')[0]['text'])){
+            $price = $dom->select('.crfFDZ')[0]['text'];
             $price = str_replace(',', '.', $price);
             $price = preg_replace('/[^,.0-9]/', '', $price);
             $price = ltrim($price, '.');
